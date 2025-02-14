32996271ed272911448bc1b2c784e60457e47ea6
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2025-02-14 22:31:47 +0100

    chore: attempt remove client from db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,3 @@
 import { drizzle } from "drizzle-orm/vercel-postgres";
-import { createClient } from "@vercel/postgres";
 
-const client = createClient({ connectionString: process.env.DATABASE_URL });
-
-export const db = drizzle(client, { casing: "snake_case" });
+export const db = drizzle();

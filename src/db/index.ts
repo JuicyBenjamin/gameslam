2720f19790e0a00631026a72779ec9a0d656b93c
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2025-02-14 15:48:13 +0100

    chore: simplify functions folder

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { createClient } from "@vercel/postgres";
 
-// Manually provide Supabase connection string
 const client = createClient({ connectionString: process.env.DATABASE_URL });
 
 export const db = drizzle(client, { casing: "snake_case" });

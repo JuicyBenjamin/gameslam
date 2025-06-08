66139740f911ebd3e6b8fe94f8ffd847a7748c0c
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2025-06-08 20:57:10 +0200

    chore(logout): try to remove the logout call from /logout

--- src/routes/logout/index.tsx
+++ src/routes/logout/index.tsx
@@ -1,8 +1,8 @@
 import type { RequestHandler } from "@builder.io/qwik-city";
-import { supabaseClient } from "~/lib/supabase";
+// import { supabaseClient } from "~/lib/supabase";
 
 export const onGet: RequestHandler = async (requestEvent) => {
-  const supabase = supabaseClient(requestEvent);
-  await supabase.auth.signOut();
+  // const supabase = supabaseClient(requestEvent);
+  // await supabase.auth.signOut();
   throw requestEvent.redirect(302, "/");
 };

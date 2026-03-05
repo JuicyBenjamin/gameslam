77c993889dc982a70da945d61ad143fdc62ff7f5
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2026-03-06 00:45:51 +0100

    chore: common database url

--- src/lib/prisma.server.ts
+++ src/lib/prisma.server.ts
@@ -1,13 +1,8 @@
 import { PrismaClient } from '@/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
-import pg from 'pg'
 
-const pool = new pg.Pool(
-  process.env.DIRECT_DATABASE_URL != null
-    ? { connectionString: process.env.DIRECT_DATABASE_URL }
-    : undefined,
-)
+const connectionString = process.env.DIRECT_DATABASE_URL ?? process.env.DATABASE_URL!
 
-const adapter = new PrismaPg(pool)
+const adapter = new PrismaPg({ connectionString })
 
 export const prisma = new PrismaClient({ adapter })

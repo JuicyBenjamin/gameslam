da9372238cd6cf22d7fb2ebeefc0a633b867e5c5
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2026-03-06 00:02:37 +0100

    chore: fallback env

--- src/lib/prisma.server.ts
+++ src/lib/prisma.server.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from '@/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DIRECT_DATABASE_URL,
-})
+const connectionString = process.env.DIRECT_DATABASE_URL ?? process.env.DATABASE_URL
+
+const adapter = new PrismaPg({ connectionString })
 
 export const prisma = new PrismaClient({ adapter })

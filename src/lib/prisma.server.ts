6225a0815626927a4d4a175064c1ae68f3def503
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2026-03-06 00:35:42 +0100

    chore: pgpool

--- src/lib/prisma.server.ts
+++ src/lib/prisma.server.ts
@@ -1,8 +1,13 @@
 import { PrismaClient } from '@/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
 
-const connectionString = process.env.DIRECT_DATABASE_URL ?? process.env.DATABASE_URL
+const pool = new pg.Pool(
+  process.env.DIRECT_DATABASE_URL != null
+    ? { connectionString: process.env.DIRECT_DATABASE_URL }
+    : undefined,
+)
 
-const adapter = new PrismaPg({ connectionString })
+const adapter = new PrismaPg({ pool })
 
 export const prisma = new PrismaClient({ adapter })

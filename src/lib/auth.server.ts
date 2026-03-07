7edd8455679717e396c91b0280ea92fe4ffcb46b
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2026-03-07 11:57:20 +0100

    chore: set deno trusted origin

--- src/lib/auth.server.ts
+++ src/lib/auth.server.ts
@@ -4,6 +4,8 @@ import { tanstackStartCookies } from 'better-auth/tanstack-start'
 import { prisma } from '@/lib/prisma.server'
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  trustedOrigins: ['https://*.deno.dev', 'https://*.deno.net'],
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
